039fd81a810cca261fd029306f46311685e52e9e
Ужаснов Сергей Владимирович <suzhasnov@alfabank.ru>
2026-07-06 13:44:10 +0300

    homework-11 is done

---
+++ src/main/java/ru/alfabank/homework11/NotEnoughWaterException.java
@@ -0,0 +1,8 @@
+package ru.alfabank.homework11;
+
+public class NotEnoughWaterException extends RuntimeException {
+
+    public NotEnoughWaterException(String message) {
+        super(message);
+    }
+}
