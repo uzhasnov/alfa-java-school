4d8d8e13002ffe13a0b2059d71bcdee1fbea944e
Ужаснов Сергей Владимирович <suzhasnov@alfabank.ru>
2026-07-06 15:22:30 +0300

    fixes for homework-12

--- src/main/java/ru/alfabank/homework12/ConveyorBeltMalfunctionError.java
+++ src/main/java/ru/alfabank/homework12/ConveyorBeltMalfunctionError.java
@@ -1,4 +1,8 @@
 package ru.alfabank.homework12;
 
-public class ConveyorBeltMalfunctionError extends RuntimeException {
+public class ConveyorBeltMalfunctionError extends Error {
+
+    public ConveyorBeltMalfunctionError(String message) {
+        super(message);
+    }
 }
--- src/main/java/ru/alfabank/homework12/InvalidBaggageWeightException.java
+++ src/main/java/ru/alfabank/homework12/InvalidBaggageWeightException.java
@@ -1,6 +1,6 @@
 package ru.alfabank.homework12;
 
-public class InvalidBaggageWeightException extends Error {
+public class InvalidBaggageWeightException extends RuntimeException {
 
     public InvalidBaggageWeightException(String message) {
         super(message);
